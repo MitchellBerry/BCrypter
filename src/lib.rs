6c01a02ef49a2689fba9a1e736740c08bb08dc2a
MitchellBerry <mitchellberry@protonmail.com>
2018-10-06 12:41:02 +0700

    b64

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,29 @@
+extern crate  base64;
+
+use blowfish::blowfish;
+use base64
+
+
+struct bcrypt_inputs {
+    password: String
+    salt : Option<[u8, 16]>,
+    cost : Option<u8>,
+}
+
+impl bcrypt_inputs{
+    pub fn new(password: String)
+} 
+
+struct output {
+    byte_digest : [u8, 24],
+    hash_string : &str
+}
+
 #[cfg(test)]
 mod tests {
+    mod lib
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        let inp  = bcrypt_inputs{"ssss"}
     }
 }
