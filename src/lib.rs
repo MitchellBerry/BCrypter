b3033e2b490aa62f3256c9b408d124078ec2ff86
MitchellBerry <mitchellberry@protonmail.com>
2018-10-10 22:49:19 +0700

    cleanup - passing tests no std

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,8 @@ extern crate alloc;
 extern crate base64;
 extern crate blowfish;
 
+pub mod b64;
+
 use rand::Rng;
 use alloc::format;
 use std::vec::Vec;
@@ -15,23 +17,9 @@ use blowfish::Blowfish;
 use std::string::String;
 use alloc::prelude::ToString;
 
-pub mod b64;
-
-use b64::{encode, decode};
-
-//use base64::{encode_config, decode_config, CRYPT};
-// use block_modes::{Ecb, BlockMode, BlockModeIv};
-// use block_modes::block_padding::ZeroPadding;
-
-// type BlowfishECB = Ecb<Blowfish, ZeroPadding>;
-
 pub fn bcrypt(password: String) -> Bcrypt{
-    let inputs = Bcrypt{password: password,
-            salt: None,
-            cost: None};
-    inputs    
+    Bcrypt{password: password, salt: None, cost: None}   
 }
-
 
 pub struct Bcrypt {
     pub password: String, 
@@ -40,27 +28,18 @@ pub struct Bcrypt {
 }
 
 impl Bcrypt{
-
     pub fn hash(self)-> Output{
         let input = self.set_defualts();
         let cost = input.cost.unwrap();
         let salt = input.salt.unwrap();
-
         let salt_b64 = b64::encode(salt.to_vec());
         let digest: [u8; 24] = hasher(input);
         let digest_b64 = b64::encode(digest[..23].to_vec()); //Remove last byte
         let hash_string = concat_hash_string(cost, &salt_b64, &digest_b64);
-        Output{ digest,
-                digest_b64,
-                salt,
-                salt_b64,
-                cost,
-                hash_string}
+        Output{ digest, digest_b64, salt, salt_b64, cost, hash_string}
     }
 
-
-
-    fn salt (self, salt: [u8; 16]) -> Bcrypt {
+    pub fn salt (self, salt: [u8; 16]) -> Bcrypt {
         Bcrypt {
             password: self.password,
             salt: Some(salt),
@@ -68,7 +47,7 @@ impl Bcrypt{
         }
     }
 
-    fn cost (self, cost: u8) -> Bcrypt {
+    pub fn cost (self, cost: u8) -> Bcrypt {
         match cost {
             4..=32 =>    Bcrypt {
                         password: self.password,
@@ -79,7 +58,7 @@ impl Bcrypt{
         }   
     }
 
-    fn set_defualts(mut self) -> Bcrypt{
+    pub fn set_defualts(mut self) -> Bcrypt{
         if self.salt == None {
             let mut rng = rand::thread_rng();
             let salt : [u8; 16] = rng.gen();
@@ -92,10 +71,6 @@ impl Bcrypt{
     }
 } 
 
-fn concat_hash_string(cost: u8, salt_b64 : &String, digest_b64: &String) -> String{
-    format!("$2b${:02}${}{}", cost, salt_b64, digest_b64)
-}
-
 pub struct Output {
     digest : [u8; 24],
     digest_b64 : String,
@@ -103,13 +78,9 @@ pub struct Output {
     salt_b64: String,
     cost: u8,
     hash_string: String
-
 }
 
-
-
 fn eks_blowfish_setup(password: &[u8], salt: &[u8;16], cost: u8) -> Blowfish {
-
     let mut state = Blowfish::bc_init_state();
     state.salted_expand_key(salt, password);
     for _ in 0..1u32 << cost {
@@ -124,36 +95,19 @@ fn hasher(inputs: Bcrypt)-> [u8; 24]{
     let salt = inputs.salt.unwrap();
     let cost = inputs.cost.unwrap();
     let mut pw_bytes = inputs.password.into_bytes();
-    
     pw_bytes.push(0);
-    let state = eks_blowfish_setup(&pw_bytes,
-                                   &salt,
-                                    cost);
-
-
+    let state = eks_blowfish_setup(&pw_bytes, &salt, cost);
     let mut ctext = [0x4f727068, 0x65616e42, 0x65686f6c,
                      0x64657253, 0x63727944, 0x6f756274];
-
     for i in (0..6).step_by(2) {
+        let j = i + 1;
         for _ in 0..64 {
-            let (l, r) = state.bc_encrypt(ctext[i], ctext[i+1]);
+            let (l, r) = state.bc_encrypt(ctext[i], ctext[j]);
             ctext[i] = l;
-            ctext[i+1] = r;
+            ctext[j] = r;
         }
-
-        // let (mut low, mut mid) = (i*4, (i+1)*4);
-        // let ctext_bytes = ctext[i].to_be_bytes();
-        // let ctext_bytes1 = ctext[i+1].to_be_bytes();
-        // for j in 0..4 {
-        //     output[low + j] = ctext_bytes[j];
-        //     output[mid + j] = ctext_bytes1[j]; 
-        // }
-        let (mut first, mut second) = (i*4, (i+1)*4);
         output.extend_from_slice(&ctext[i].to_be_bytes());
-        output.extend_from_slice(&ctext[i+1].to_be_bytes());
-        
-        //write_u32_be(&mut output[i * 4..(i + 1) * 4], ctext[i]);
-        //write_u32_be(&mut output[(i + 1) * 4..(i + 2) * 4], ctext[i + 1]);
+        output.extend_from_slice(&ctext[j].to_be_bytes());
     }
     output_vec_to_array(output)
 }
@@ -166,34 +120,33 @@ fn output_vec_to_array(vec : Vec<u8>) -> [u8; 24] {
     out
 }
 
-fn salt_vec_to_array(vec : Vec<u8>) -> [u8; 16] {
-    let mut out = [0u8; 16];
-    for (i, slice) in vec.iter().enumerate(){
-        out[i] = *slice;
-    }
-    out
+fn concat_hash_string(cost: u8, salt : &String, digest: &String) -> String{
+    format!("$2b${:02}${}{}", cost, salt, digest)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    fn salt_vec_to_array(vec : Vec<u8>) -> [u8; 16] {
+        let mut out = [0u8; 16];
+        for (i, slice) in vec.iter().enumerate(){
+            out[i] = *slice;
+        }
+        out
+    }
+
     #[test]
     fn it_works() {
         
-        let saltvec = decode("EGdrhbKUv8Oc9vGiXX0HQO".to_string());
+        let saltvec = b64::decode("EGdrhbKUv8Oc9vGiXX0HQO".to_string());
         let a : &[u8] = saltvec.as_ref();
-        let mut salt_test = [0u8; 16]; 
-        let mut result = bcrypt(String::from("correctbatteryhorsestapler"))
-                            .cost(4);
-                            //.salt(salt_vec_to_array(saltvec.clone()));
+        let result = bcrypt(String::from("correctbatteryhorsestapler"))
+                            .cost(4)
+                            .salt(salt_vec_to_array(saltvec.clone()));
         let out = result.hash();
         //println!("{}", out.hash_string);
         //"$2b$04$EGdrhbKUv8Oc9vGiXX0HQOxSg445d458Muh7DAHskb6QbtCvdxcie"
-        let a = 1;
+        let _a = 1;
     }
-
-    // #[test]
-    // fn b() {
-    //     unimplemented!();
-    // }
 }
