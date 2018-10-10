7161697160d6bbb6056d7bcaf39bfaac5d518bb5
MitchellBerry <mitchellberry@protonmail.com>
2018-10-11 02:17:18 +0700

    added error types

--- src/errors.rs
+++ src/errors.rs
@@ -1,19 +1,20 @@
-#[no_std]
+#![no_std]
 
 extern crate std;
 
-use std::error;
+use std::error::Error;
 
+#[derive(Debug)]
 enum BcryptError {
     InvalidCost(Error),
-    VerifyFailed(Err)
+    VerifyFailed(Error)
 }
 
 impl fmt::Display for BcryptError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match *self {            
             BcryptError::InvalidCost(ref err) => write!(f, "Cost must be between 4-31: {}", err),
-            CliError::Parse(ref err) => write!(f, "Parse error: {}", err),
+            BcryptError::VerifyFailed(ref err) => write!(f, "Password does not match: {}", err),
         }
     }
 }
