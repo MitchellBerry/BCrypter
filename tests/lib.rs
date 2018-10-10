6fca19015da9138fb97877a6c7a318458846df26
MitchellBerry <mitchellberry@protonmail.com>
2018-10-10 23:04:27 +0700

    created tests folder

---
+++ tests/lib.rs
@@ -0,0 +1,25 @@
+#[cfg(test)]
+
+use super::*;
+
+fn salt_vec_to_array(vec : Vec<u8>) -> [u8; 16] {
+    let mut out = [0u8; 16];
+    for (i, slice) in vec.iter().enumerate(){
+        out[i] = *slice;
+    }
+    out
+}
+
+#[test]
+fn it_works() {
+    
+    let saltvec = b64::decode("EGdrhbKUv8Oc9vGiXX0HQO".to_string());
+    let a : &[u8] = saltvec.as_ref();
+    let result = bcrypt(String::from("correctbatteryhorsestapler"))
+                        .cost(4)
+                        .salt(salt_vec_to_array(saltvec.clone()));
+    let out = result.hash();
+    //println!("{}", out.hash_string);
+    //"$2b$04$EGdrhbKUv8Oc9vGiXX0HQOxSg445d458Muh7DAHskb6QbtCvdxcie"
+    let _a = 1;
+}
