8590e235b0242847d3262135ceb32a5a39e9e70c
MitchellBerry <mitchellberry@protonmail.com>
2018-10-19 11:54:57 +0700

    added basic benchmark

--- benches/lib.rs
+++ benches/lib.rs
@@ -1,5 +1,6 @@
 #![no_std]
 #![feature(test)]
+#![feature(alloc)] 
 
 extern crate alloc;
 extern crate bcrypt;
@@ -9,13 +10,13 @@ use bcrypt::*;
 use test::Bencher;
 use alloc::string::String;
 
-fn basic_password_cost_12() {
+fn basic_password_cost_4() {
     let pw_bytes = String::from("password");
     let hasher = password(pw_bytes);    
-    assert!(hasher.cost(12).hash().is_ok())
+    assert!(hasher.cost(4).hash().is_ok())
 }
 
 #[bench]
 fn bench_basic_password(b: &mut Bencher){
-    b.iter(|| basic_password_cost_12())
+    b.iter(|| basic_password_cost_4())
 }
