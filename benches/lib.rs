bd10008d3cea84caa7a661f4b19e0075c2d53d35
MitchellBerry <mitchellberry@protonmail.com>
2018-10-19 11:50:59 +0700

    .

--- benches/lib.rs
+++ benches/lib.rs
@@ -3,14 +3,19 @@
 
 extern crate alloc;
 extern crate bcrypt;
+extern crate test;
 
 use bcrypt::*;
 use test::Bencher;
 use alloc::string::String;
 
-#[bench]
 fn basic_password_cost_12() {
     let pw_bytes = String::from("password");
     let hasher = password(pw_bytes);    
     assert!(hasher.cost(12).hash().is_ok())
+}
+
+#[bench]
+fn bench_basic_password(b: &mut Bencher){
+    b.iter(|| basic_password_cost_12())
 }
